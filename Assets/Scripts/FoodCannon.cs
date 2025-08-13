d29b986b61c17cebc35d3a818a6f0608713289a2
NicoCoolCode <blni@coolcode.io>
2025-08-13 18:17:33 +0200

    man kann Croissants schiesen

--- Assets/Scripts/FoodCannon.cs
+++ Assets/Scripts/FoodCannon.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class FoodCannon : MonoBehaviour
 {
+    [SerializeField] private GameObject food;
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject newFood =Instantiate(food, transform.position, transform.rotation);
+            newFood.GetComponent <Rigidbody>().velocity = Vector3.forward*100;
 
+        }
+    }
 }
