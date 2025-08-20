ce12db2e464c29684ee480b3222379fc2ff64479
NicoCoolCode <blni@coolcode.io>
2025-08-20 17:51:40 +0200

    Croissant fliegt dahin wo man zielt

--- Assets/Scripts/FoodCannon.cs
+++ Assets/Scripts/FoodCannon.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class FoodCannon : MonoBehaviour
 {
     [SerializeField] private GameObject food;
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private float shootPower;
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            GameObject newFood =Instantiate(food, transform.position, transform.rotation);
-            newFood.GetComponent <Rigidbody>().velocity = Vector3.forward*100;
+            GameObject newFood =Instantiate(food, shootPoint.position, shootPoint.rotation);
+            newFood.GetComponent<Rigidbody>().velocity = shootPoint.TransformDirection(Vector3.forward * shootPower);
 
         }
     }
