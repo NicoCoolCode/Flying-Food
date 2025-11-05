d2d42198a9d3ed0cd56d50cf7fdc8433f4a9bcfd
NicoCoolCode <blni@coolcode.io>
2025-11-05 18:07:48 +0100

    es kommt ein times up text wenn der timer auf 0 ist und er hört bei 0 auf

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,32 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private int timer=60;
+    [SerializeField] private TMP_Text timerText;
+    [SerializeField] private TMP_Text timesUpText;
     private void Start()
     {
         InvokeRepeating(nameof(Countdown),1,1);
+        timerText.text = "timer; "+  timer.ToString();
     }
 
     private void Countdown()
-    {
+    {   
         timer--;
-        print(timer);
+        timerText.text = "timer; "+ timer.ToString();
+        if (timer == 0)
+        {
+            timesUpText.gameObject.SetActive(true);
+            Invoke(nameof(GoToMenu),3);
+            CancelInvoke(nameof(Countdown));
+        }
+    }
+
+    private void GoToMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
     }
 }
