90689da5f08c2ba6522a06ea76d351b7d2dae224
NicoCoolCode <blni@coolcode.io>
2025-11-12 16:25:52 +0100

    musik wird nicht zerstört

--- Assets/Scripts/DontDestroyEvenThoughUnityDoesNotLikeThisName.cs
+++ Assets/Scripts/DontDestroyEvenThoughUnityDoesNotLikeThisName.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class DontDestroyEvenThoughUnityDoesNotLikeThisName : MonoBehaviour
 {
+    private static DontDestroyEvenThoughUnityDoesNotLikeThisName instance;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (instance==null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
